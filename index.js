258bcde4a1c7a2ed83c407fecddff42685fbf213
Abed Elhadi Bhuty <30442674+AbedElhadiBhoti@users.noreply.github.com>
2018-12-29 16:57:12 +0200

    add css and js files

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.querySelector('button').addEventListener('click', function () {
   const x = document.querySelector('input').value
   const h1 = document.querySelector('h1')
-  h1.innerHTML = x
+  h1.textContent = x
 })
