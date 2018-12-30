7e1a86a749f13a1d0e7794a434535fbb83109ba1
Abed Elhadi Bhuty <30442674+AbedElhadiBhoti@users.noreply.github.com>
2018-12-30 15:38:13 +0200

    update the heading with value from the form

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-document.querySelector('button').addEventListener('click', function () {
-  const x = document.querySelector('input').value
-  const h1 = document.querySelector('h1')
-  h1.textContent = x
-})
+const form = document.querySelector('form#userForm')
+const handleSubmit = function (ev) {
+  ev.preventDefault()
+  const heading = document.querySelector('h1')
+  const input = document.querySelector('input')
+  heading.textContent = input.value
+}
+form.addEventListener('submit', handleSubmit)
