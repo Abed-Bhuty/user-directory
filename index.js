8806820b647249e05aeee2b169fbf323a6434be0
Abed Elhadi Bhuty <30442674+AbedElhadiBhoti@users.noreply.github.com>
2019-01-01 09:33:58 +0200

    add multiple names , clearing the input each time

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const handleSubmit = function (ev) {
   ev.preventDefault()
   const form = ev.target
   const userName = form.userName.value
-  const heading = document.querySelector('h1')
-  heading.textContent = userName
+  const users = document.querySelector('#users')
+  users.textContent += ' ' + userName
+  form.userName.value = ''
 }
 form.addEventListener('submit', handleSubmit)
