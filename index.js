7145f59e51bebddd74cd5d4b710e96b7458f036f
Abed Elhadi Bhuty <30442674+AbedElhadiBhoti@users.noreply.github.com>
2019-01-02 18:09:35 +0200

    append the user as proper DOM element

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const form = document.querySelector('form#userForm')
-const handleSubmit = function (ev) {
-  ev.preventDefault()
-  const form = ev.target
-  const userName = form.userName.value
-  const age = form.age.value
-  const users = document.querySelector('#users')
-  users.innerHTML += `<p>${userName}, ${age}</p>`
-  form.reset()
-  form.userName.focus()
-}
-form.addEventListener('submit', handleSubmit)
+const form = document.querySelector("form#userForm");
+const handleSubmit = function(ev) {
+  ev.preventDefault();
+  const form = ev.target;
+  const userName = form.userName.value;
+  const age = form.age.value;
+  const users = document.querySelector("#users");
+  const p = document.createElement("p");
+  p.textContent = `${userName}, ${age}`;
+  users.appendChild(p);
+  form.reset();
+  form.userName.focus();
+};
+form.addEventListener("submit", handleSubmit);
