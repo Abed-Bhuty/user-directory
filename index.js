d566abfc4c41fbfb05a512afee51e08b67c7e6e7
Abed Elhadi Bhuty <30442674+AbedElhadiBhoti@users.noreply.github.com>
2019-01-05 12:47:33 +0200

    Display color in an ebmty div

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("form#userForm");
-const handleSubmit = function (ev) {
+const handleSubmit = function(ev) {
   ev.preventDefault();
   const form = ev.target;
 
@@ -7,23 +7,25 @@ const handleSubmit = function (ev) {
   const age = form.age.value;
   const favoritecolor = form.favoritecolor.value;
 
-  const users = document.querySelector('#users')
+  const users = document.querySelector("#users");
 
-
-  const list = document.createElement('ul')
-  const nameItem = document.createElement('li')
-  nameItem.textContent = `Name: ${userName}`
-  const ageItem = document.createElement('li')
-  ageItem.textContent = `Age: ${age}`
-  const colorItem = document.createElement('li')
-  colorItem.textContent = `color: ${favoritecolor}`
-
+  const list = document.createElement("ul");
+  const nameItem = document.createElement("li");
+  nameItem.textContent = `Name: ${userName}`;
+  const ageItem = document.createElement("li");
+  ageItem.textContent = `Age: ${age}`;
+  const colorItem = document.createElement("li");
+  colorItem.textContent = `Favorite Color: `;
+  const colorDiv = document.createElement("div");
+  colorDiv.style.backgroundColor = favoritecolor;
+  colorDiv.style.width = "2rem";
+  colorDiv.style.height = "2rem";
+  colorItem.appendChild(colorDiv);
 
   list.appendChild(nameItem);
-  list.appendChild(ageItem)
-  list.appendChild(colorItem)
-  colorItem.style.backgroundColor = favoritecolor;
-  users.appendChild(list)
+  list.appendChild(ageItem);
+  list.appendChild(colorItem);
+  users.appendChild(list);
 
   form.reset();
   form.userName.focus();
